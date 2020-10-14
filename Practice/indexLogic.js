3e419cf872e864429ba5a76d734d1825a4678af6
Ranjiro <yoppadayo@icloud.com>
2020-10-14 05:26:46 +0000

    kadai-02

---
+++ Practice/indexLogic.js
@@ -0,0 +1,4 @@
+const n = 9
+const isGusu = false
+const is3x = true
+isGusu && is3x
---
+++ kadai-02/index2.js
@@ -0,0 +1,13 @@
+const firstName = 'Ranjiro'
+const familyName = 'Hayashi'
+console.log(firstName + familyName)
+
+const number = 1+2+3+4+5+6+7+8+9+10
+console.log(number)
+
+const dog = true
+const cat = false
+console.log(dog||cat)
+console.log(dog&&cat)
+console.log(!dog&&!cat)
+console.log(!(dog||cat))

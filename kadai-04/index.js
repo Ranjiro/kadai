59704cadeb20ddb1770fe6f4048342f3fb6a41b1
Ranjiro <yoppadayo@icloud.com>
2020-10-21 05:01:05 +0000

    kadai-04

---
+++ kadai-04/index.js
@@ -0,0 +1,12 @@
+const product = (a, b)=>{
+    return a * b
+}
+console.log(product(107,109))
+
+const sum = (n) => {
+    return (n+1) * n /2
+}
+console.log(sum(500))
+
+setTimeout(() => {console.log('hello')},3000)
+setTimeout(() => {console.log('bye')},5000)
